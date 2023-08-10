508a868472c1e4885d65976878b2a39d106efaa3
Adeniyi02 <niyiadejare02@gmail.com>
2023-08-10 16:48:53 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
-int main() {
-  printf("Programming is like building a multilingual puzzle, \n");
-  return 0;
+/**
+ *  main -Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-#include <stdio.h>
 
-int main() {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return 0;
-}
