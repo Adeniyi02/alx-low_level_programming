797ea3cf5a3278b99e5e53179b92b2ea3ea63572
Adeniyi02 <niyiadejare02@gmail.com>
2023-08-21 16:19:27 -0700

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,18 @@
-#include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code
+ * _strlen - Calculates the length of a string.
+ * @s: Pointer to the input string.
  *
- * Return: Always 0.
+ * Return: The length of the string.
  */
-int main(void)
+int _strlen(char *s)
 {
-    char *str;
-    int len;
+    int length = 0; 
 
-    str = "My first strlen!";
-    len = _strlen(str);
-    printf("%d\n", len);
-    return (0);
+    while (*s != '\0') 
+    {
+        length++; 
+        s++;      
+    }
+
+    return length;
 }
