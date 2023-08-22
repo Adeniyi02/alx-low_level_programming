0be9b220459add788334b803867eaf9c817ecfe3
Adeniyi02 <niyiadejare02@gmail.com>
2023-08-21 17:51:33 -0700

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,15 @@
 #include "main.h"
-
 /**
- * puts2 - Prints every other character of a string, starting with the first
- * character, followed by a new line.
- * @str: Pointer to the input string.
+ * puts2 - prints every other character of a string~
+ * @str: char to check
+ * Return: 0 is success
  */
 void puts2(char *str)
 {
-	int i;
+	int string;
 
-	for (i = 0; str[i] != '\0'; i += 2)
-		_putchar(str[i]);
-
+	for (string = 0; str[string] != '\0'; string++)
+	if (string % 2 == 0)
+		_putchar(str[string]);
 	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,23 +1,19 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * print_array - Prints n elements of an array of integers, followed by a new line.
- * @a: Pointer to the input array of integers.
- * @n: Number of elements to be printed.
+ * print_array - prints n element of an array of integers
+ * @a: int to check
+ * @n: int to check
+ * Return: 0 is success
  */
 void print_array(int *a, int n)
 {
-	int i;
+	int x;
 
-	for (i = 0; i < n; i++)
-	
-	{
-		printf("%d", a[i]);
-		if (i != n - 1)
-			printf(", ");
-	}
-
-
-	printf("\n");
+	for (x = 0; x < n; x++)
+		if (x != n - 1)
+		printf("%d, ", a[x]);
+		else
+			printf("%d", a[x]);
+printf("\n");
 }
