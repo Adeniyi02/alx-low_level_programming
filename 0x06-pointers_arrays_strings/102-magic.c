72997b6df3682058133c5cf2d9cef0cd3c0710d3
Adeniyi02 <niyiadejare02@gmail.com>
2023-08-23 16:43:33 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -7,11 +7,14 @@
  */
 int main(void)
 {
-    int a[] = {0, 1, 2, 3, 4};
-    int *p;
+    {
+  int n;
+  int a[5];
+  int *p;
 
-    p = &a[2];
-    *(p + 2) = 9;
-    printf("a[2] = %d\n", a[2]);
-    return (0);
+  a[2] = 1024;
+  p = &n;
+   *(p + 5) = 98;
+   printf("a[2] = %d\n", a[2]);
+  return (0);
 }
