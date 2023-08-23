00ca4df98fda5f1971aa08ea81b69981c5ac7d7c
Adeniyi02 <niyiadejare02@gmail.com>
2023-08-23 16:56:42 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
-
-/**
- * main - Entry point
- * Return: Always 0
- */
 int main(void)
 {
-    {
   int n;
   int a[5];
   int *p;
 
   a[2] = 1024;
   p = &n;
-   
-    
-    *(p + 5) = 98;
-    
-    printf("a[2] = %d\n", a[2]);
+  /*
+   * write your line of code always for easy identification
+   * notice:
+   * - do not use a as a character
+   * - you are not allowed to modify p
+   * - just a single statement
+   * - you are not allowed to code anything else other than this task code
+   */
+  *(p + 5) = 98;
+  /* thereby, machine can print 98\n */
+  printf("a[2] = %d\n", a[2]);
   return (0);
 }
