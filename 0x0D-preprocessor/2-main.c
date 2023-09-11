1fcbe144cb481802c0befbd83c398aa14fe02397
Adeniyi02 <niyiadejare02@gmail.com>
2023-09-11 14:27:39 -0700

    C - Proprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,46 +1,12 @@
-#include <stdlib.h>
-
+#include <stdio.h>
 /**
- * main - Entry point
+ *main - prints the name of the file
  *
- * Return: Always 0 (Success)
- */
+ *Return: always 0
+*/
+
 int main(void)
 {
-    char *file_name = __FILE__;
-    int i = 0;
-
-    while (file_name[i] != '\0')
-    {
-        if (file_name[i] == '/')
-        {
-            file_name = file_name + i + 1;
-            break;
-        }
-        i++;
-    }
-
-    char *result = malloc(1024 * sizeof(char));
-    if (result == NULL)
-        exit(EXIT_FAILURE);
-
-    int j = 0;
-    while (file_name[j] != '\0')
-    {
-        result[j] = file_name[j];
-        j++;
-    }
-    result[j] = '\n';
-    result[j + 1] = '\0';
-
-    char *ptr = result;
-    while (*ptr)
-    {
-        write(1, ptr, 1);
-        ptr++;
-    }
-
-    free(result);
-
-    exit(EXIT_SUCCESS);
+	printf("%s\n", __FILE__);
+	return (0);
 }
